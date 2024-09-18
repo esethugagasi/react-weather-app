e4301d6339c1464086766dce2dd8062efaa0f08e
esethugagasi <esethugagasi14@gmail.com>
2024-09-18 13:48:23 +0200

    Aadded forecast for 6 days

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
 ReactDOM.render(
   <React.StrictMode>
